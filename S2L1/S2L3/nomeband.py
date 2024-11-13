32e7d2ea48c9788c74fe98c06c5c70dec55d3740
Noel2244 <noelpetralia2244@gmail.com>
2024-11-13 15:19:38 +0100

    Delete S2L1/S2L3 directory

--- S2L1/S2L3/nomeband.py
+++
@@ -1,4 +0,0 @@
-città = input("Dimmi il nome della tua città: ")
-animale = input("Dimmi ilnome del tuo animale: ")
-città_animale= città + animale
-print(f"Il nome della tua band sarà: {città_animale}")
