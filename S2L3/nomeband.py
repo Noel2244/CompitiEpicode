d51187db7e10027f1f28110844b8b3c95a2e4a44
Noel Petralia <noelpetralia@2244gmail.com>
2024-11-13 10:27:05 -0500

    Esercizio 13.11.2024

---
+++ S2L3/nomeband.py
@@ -0,0 +1,9 @@
+città = input("Dimmi il nome della tua città: ")
+animale = input("Dimmi ilnome del tuo animale: ")
+città_animale= città + animale
+print(f"Il nome della tua band sarà: {città_animale}")
+piace= input("Ti piace :")
+if piace == "si":
+    print("Mi fa molto piacere")
+elif piace == "no":
+    print("Allora mi sa che lo cambierò") 
